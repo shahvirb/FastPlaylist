38c6d201860117ae5b51880d678c3d8b2e50d8e9
Shahvir <shahvirb@gmail.com>
2017-12-31 16:22:36 -0600

    fleshing out basic search and filtering support

--- cli.py
+++ cli.py
@@ -2,12 +2,38 @@ import gmusicapi
 import logging
 import getpass
 import gcredentials
+import jellyfish
 
 logging.basicConfig(level=logging.DEBUG)
 
-if __name__ == '__main__':
+def get_authenticated_client():
     gmapi = gmusicapi.Mobileclient()
     email = gcredentials.EMAIL_HARDCODE
     logging.info("Attempting login as {} with android ID {}".format(email, gcredentials.AID_HARDCODE))
     success = gmapi.login(email, getpass.getpass(), gcredentials.AID_HARDCODE)
     logging.info('Logged in: {}'.format(success))
+    return gmapi
+
+
+def find_songs(results, filter):
+    tracks = []
+    for hit in results['song_hits']:
+        track = hit['track']
+        keys = set(filter.keys()).intersection(track.keys())
+        matches = [jellyfish.match_rating_comparison(track[key], filter[key]) for key in keys]
+        if not False in matches:
+            tracks.append(track)
+    return tracks
+
+
+if __name__ == '__main__':
+    gmapi = get_authenticated_client()
+    #search = input('Search: ')
+    filter = {'title': 'Sweet Child O\' Mine',
+              'artist':'guns n\ roses',
+              'album': 'Appetite for Destruction'}
+    search = filter['title']
+    results = gmapi.search(search)
+    tracks = find_songs(results, filter)
+    import pprint
+    pprint.pprint(tracks[0:2])
