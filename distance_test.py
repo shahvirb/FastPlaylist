166d06216bc3c8005d4ba184d53bd5a58956ee6b
Shahvir <shahvirb@gmail.com>
2017-12-31 22:54:25 -0600

    more test cases

--- distance_test.py
+++ distance_test.py
@@ -1,7 +1,14 @@
 import jellyfish
 
 TESTS = [('Chronicle: 20 Greatest Hits', 'chronicle'),
-         ('Guns N\' Roses', 'GUNS N Roses')]
+         ('Guns N\' Roses', 'GUNS N Roses'),
+         ('Santana', 'Carlos Santana')]
+
+s2 = 'a thousand kisses deep'
+s1 = ['Sailing to Byzantium', 'Pelt', 'Romance and Revolution', 'House of Women', 'A Thousand Kisses Deep', 'Yell of the Gazelle', 'You Draw The Line', 'You Draw the Line', 'Secret Life Of A Girl', 'Tapestry Unravelled', 'Aililiu', 'Songs Of Leonard Cohen', 'Lovers And Strangers']
+
+s1 = ['The Antisocial Club', 'The Imagined Savior Is Far Easier To Paint', 'When The Heart Emerges Glistening']
+TESTS = [(string.lower(), s2.lower()) for string in s1]
 
 ALGOS = {'levenshtein_distance': jellyfish.levenshtein_distance,
          'damerau_levenshtein_distance': jellyfish.damerau_levenshtein_distance,
@@ -20,4 +27,5 @@ def test(case, ALGOS):
 if __name__ == '__main__':
     for case in TESTS:
         test(case, ALGOS)
+        print()
         #test((case[1], case[0]), ALGOS)
