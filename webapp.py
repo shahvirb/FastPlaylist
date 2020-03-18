374e9c5aa74f5a71404890766ee1f3782b9a1cfa
shahvirb <shahvirb@gmail.com>
2020-03-18 16:45:54 -0500

    remove card outlines. Wider submit button

--- webapp.py
+++ webapp.py
@@ -4,8 +4,7 @@ import find_parsers
 
 logging.basicConfig(level=logging.DEBUG)
 
-app = flask.Flask(__name__, template_folder='templates',
-                  static_folder=r'C:\Users\fusiv\Documents\Progr_Proj\Python\FastPlaylist\static')
+app = flask.Flask(__name__, template_folder='templates')
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 
 
